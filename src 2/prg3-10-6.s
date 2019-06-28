e0694922d703e7fe860b7f70e78b5cf207f38c82
Lucas Magalhaes <lucas.magalhaes1985@gmail.com>
2019-06-28 10:37:11 -0300

    Changes src

--- src 2/prg3-10-6.s
+++
@@ -1,8 +0,0 @@
-	.text
-	.globl main
-
-main:
-	MOV r0, #0xFFFFFFFE
-	ADD r1, r0, r0, ASR #31
-	EOR r0, r1, r0, ASR #31
-	SWI #0x123456
--- src 2/umuls_3.s
+++
@@ -1,7 +0,0 @@
-	.text
-	.global main
-main:
-   	LDR r0, =0xFFFFFFFF
-	LDR r1, =0x80000000
-	UMULL r3, r4, r0, r1
-	SWI 0x00123456
